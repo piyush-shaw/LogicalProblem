5b4ae2f652373e5888a4c739575697d033fa2893
piyush-shaw <piyushshaw0000@gmail.com>
2023-01-12 00:01:50 +0530

    to check number is prime or not

---
+++ LogicalProgram/PrimeNumber.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace LogicalProgram
+{
+	internal class PrimeNumber
+	{
+		public static void Prime()
+		{
+			Console.WriteLine("Enter the number to check Prime ");
+			int number = Convert.ToInt32(Console.ReadLine());
+			bool isPrime = true;
+			for(int i = 2; i <= number / 2; i++)
+			{
+				if (number % i == 0)
+				{
+					isPrime = false;
+					break;
+				}
+			}
+			if (isPrime)
+			{
+				Console.WriteLine("It is Prime Number");
+			}
+			else
+			{
+				Console.WriteLine("It is not a Prime Number");
+			}
+		}
+	}
+}
+
--- LogicalProgram/Program.cs
+++ LogicalProgram/Program.cs
@@ -5,7 +5,7 @@ internal class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Welcome to Logical Problems");
-        Console.WriteLine("\n1.Fibonacci Series\n2.Perfect Number");
+        Console.WriteLine("\n1.Fibonacci Series\n2.Perfect Number\n3.PrimeNumber");
         int option = Convert.ToInt32(Console.ReadLine());
         switch (option)
         {
@@ -15,6 +15,9 @@ internal class Program
                 break;
             case 2:
                 PerfectNumber.Number();
+                break;
+            case 3:
+                PrimeNumber.Prime();
                 break;
             default:
                 Console.WriteLine("Choose from above options only ");
