38aeb3b055b2ff9511540aedacdabb7c5c590875
piyush-shaw <piyushshaw0000@gmail.com>
2023-01-11 21:55:19 +0530

    to print fibonacci series in which next term is sum of previous two terms

---
+++ LogicalProgram/FibonacciSeries.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace LogicalProgram
+{
+	internal class FibonacciSeries
+	{
+		public static void Series()
+		{
+			int a = 0, b = 1, c;
+			Console.WriteLine("Enter a number of element ");
+			int num = Convert.ToInt32(Console.ReadLine());
+			Console.Write(a + "," + b);
+			for(int i = 2; i <= num; i++)
+			{
+				c = a + b;
+				Console.WriteLine("," + c);
+				a = b;
+				b = c;
+			}
+		}
+	}
+}
+
--- LogicalProgram/Program.cs
+++ LogicalProgram/Program.cs
@@ -1,7 +1,21 @@
-﻿internal class Program
+﻿using LogicalProgram;
+
+internal class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Welcome to Logical Problems");
+        Console.WriteLine("\n1.Fibonacci Series");
+        int option = Convert.ToInt32(Console.ReadLine());
+        switch (option)
+        {
+            //Fibonacci Series
+            case 1:
+                FibonacciSeries.Series();
+                break;
+            default:
+                Console.WriteLine("Choose from above options only ");
+                break;
+        }
     }
 }
